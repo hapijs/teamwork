d24a338b7db8df4e98297fc46078cd2ce2dc7456
Eran Hammer <eran@hammer.io>
2017-10-25 21:46:26 -0700

    New interface. Closes #6

--- test/index.js
+++ test/index.js
@@ -22,48 +22,19 @@ const it = lab.test;
 
 describe('Team', () => {
 
-    it('resolve when all members finish', async () => {
+    it('resolve when meeting is attended', async () => {
 
         const team = new Teamwork.Team();
 
-        let count = '';
-        setTimeout(team.member(() => {
-
-            count += '1';
-        }), 100);
-
-        setTimeout(team.member(() => {
-
-            count += '2';
-        }), 150);
-
-        await team.work;
-        expect(count).to.equal('12');
-    });
-
-    it('resolve when all meetings are attended', async () => {
-
-        const team = new Teamwork.Team();
-        team.meetings(2);
-
-        let count = '';
         setTimeout(() => {
 
-            count += '1';
             team.attend();
         }, 100);
 
-        setTimeout(() => {
-
-            count += '2';
-            team.attend();
-        }, 150);
-
         await team.work;
-        expect(count).to.equal('12');
     });
 
-    it('resolve when all meetings are attended (constructor options)', async () => {
+    it('resolve when all meetings are attended', async () => {
 
         const team = new Teamwork.Team({ meetings: 2 });
 
@@ -82,5 +53,53 @@ describe('Team', () => {
 
         await team.work;
         expect(count).to.equal('12');
+    });
+
+    it('resolve with a note', async () => {
+
+        const team = new Teamwork.Team();
+
+        setTimeout(() => {
+
+            team.attend('1');
+        }, 100);
+
+        const note = await team.work;
+        expect(note).to.equal('1');
+    });
+
+    it('resolve with notes', async () => {
+
+        const team = new Teamwork.Team({ meetings: 2 });
+
+        setTimeout(() => {
+
+            team.attend('1');
+        }, 100);
+
+        setTimeout(() => {
+
+            team.attend('2');
+        }, 150);
+
+        const notes = await team.work;
+        expect(notes).to.equal(['1', '2']);
+    });
+
+    it('rejects on first error', async () => {
+
+        const team = new Teamwork.Team({ meetings: 2 });
+
+        setTimeout(() => {
+
+            team.attend(new Error('boom'));
+        }, 100);
+
+        setTimeout(() => {
+
+            team.attend('2');
+        }, 150);
+
+        await expect(team.work).to.reject('boom');
     });
 });
